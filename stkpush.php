9156e81e423d788892904d40889a3a880fc963c4
gadson <gadsonmulonga@gmail.com>
2023-05-17 10:19:44 +0300

    working stkpush

--- stkpush.php
+++ stkpush.php
@@ -4,8 +4,13 @@ include 'accessToken.php';
 
 date_default_timezone_set('Africa/Nairobi');
 $processrequestUrl = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+
+
 // live link to test the callback url
-$callbackurl = '';
+$callbackurl = 'https://www.lmkenya.org/callback.php';
+// $callbackurl = 'https://umeskiasoftwares.com/darajaapp/callback.php';
+
+
 // get the pass key from > mpesa express > simulator
 $passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919';
 
@@ -19,27 +24,41 @@ $phone = '254795370669';
 $money = '1';
 $PartyA = $phone;
 // PAYBILL NUMBER
-$PartyB = '';
+$PartyB = '174379';
 // ACCOUNT NUMBER
-$AccountReference = '';
-$TransactionDesc = '';
+$AccountReference = 'Test';
+$TransactionDesc = 'Test';
 $Amount = $money;
-$stkpushheader = ['Content-Type:application;/json', 'Authorization:Bearer ' . $access_token];
+$stkpushheader = ['Content-Type:application/json', 'Authorization:Bearer ' . $access_token ];
+
 
 // INITIATE THE CURL
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL, $processrequestUrl);
-curl_setopt($curl, CURLOPT_HEADER, $stkpushheader);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $stkpushheader);
 $curl_post_data = array(
-    "BusinessShortCode": $BusinessShortCode,    
-    "Password": $Password,    
-    "Timestamp":$Timestamp,    
-    "TransactionType": "CustomerPayBillOnline",    
-    "Amount": $Amount,    
-    "PartyA":$PartyA,    
-    "PartyB":$BusinessShortCode,    
-    "PhoneNumber":$PartyA,    
-    "CallBackURL": $callbackurl,    
-    "AccountReference":$AccountReference,    
-    "TransactionDesc":$TransactionDesc,
+    "BusinessShortCode" => "174379",    
+    "Password" => $Password,    
+    "Timestamp" => $Timestamp,    
+    "TransactionType" => "CustomerPayBillOnline",    
+    "Amount" => $Amount,    
+    "PartyA" => $PartyA,    
+    "PartyB" => "174379",    
+    "PhoneNumber" => $PartyA,    
+    "CallBackURL" => $callbackurl,    
+    "AccountReference" => $AccountReference,    
+    "TransactionDesc" => $TransactionDesc,
 );
+
+$data_string = json_encode($curl_post_data);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+$curl_response = curl_exec($curl);
+
+echo $curl_response;
+
+
+
+
+
